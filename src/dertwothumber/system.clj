1a3f5112b202bd5573e1a3da0868e9972657833a
Topper Bowers <topper@toppingdesign.com>
2016-02-24 20:56:02 -0500

    add cookie store and change example to use cookie store

--- src/dertwothumber/system.clj
+++ src/dertwothumber/system.clj
@@ -8,17 +8,21 @@
             [meta-merge.core :refer [meta-merge]]
             [ring.component.jetty :refer [jetty-server]]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [ring.middleware.session :refer [wrap-session]]
             [ring.middleware.webjars :refer [wrap-webjars]]
-            [dertwothumber.endpoint.example :refer [example-endpoint]]))
+            [dertwothumber.endpoint.example :refer [example-endpoint]])
+  (:use [ring.middleware.session.cookie]))
 
 (def base-config
   {:app {:middleware [[wrap-not-found :not-found]
                       [wrap-webjars]
                       [wrap-defaults :defaults]
-                      [wrap-route-aliases :aliases]]
+                      [wrap-route-aliases :aliases]
+                      [wrap-session :store-options]]
          :not-found  (io/resource "dertwothumber/errors/404.html")
          :defaults   (meta-merge site-defaults {:static {:resources "dertwothumber/public"}})
-         :aliases    {"/" "/index.html"}}})
+         :aliases    {"/" "/index.html"}
+         :store-options {:store (cookie-store {:key "a 16-byte secret"})}}})
 
 (defn new-system [config]
   (let [config (meta-merge base-config config)]
