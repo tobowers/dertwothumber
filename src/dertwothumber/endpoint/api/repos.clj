b91fec1490625d9229741097a178fc510f36167a
Topper Bowers <topper@toppingdesign.com>
2016-03-13 18:26:45 +0100

    json encode the repos

--- src/dertwothumber/endpoint/api/repos.clj
+++ src/dertwothumber/endpoint/api/repos.clj
@@ -1,12 +1,14 @@
 (ns dertwothumber.endpoint.api.repos
   (:require [compojure.core :refer :all]
             [clj-http.client :as http]
-            [tentacles.repos :as repos])
+            [tentacles.repos :as repos]
+            [cheshire.core :refer :all])
   (:use [ring.middleware.session]))
 
 (defn repos-endpoint [config]
   (context "/api" []
     (GET "/repos" {session :session} []
-      (let [user (:user session)]
-        (repos/user-repos (:login user))))))
+      (let [user  (:user session)
+            repos (repos/user-repos (:login user) {:oauth-token (:access-token session)})]
+        (generate-string repos)))))
 
