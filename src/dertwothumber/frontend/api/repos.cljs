6e44e4fb4646557a6bd8fbb8da31080ea685b1d6
Topper Bowers <topper@toppingdesign.com>
2016-03-13 18:27:12 +0100

    return a clojure data structure to a channel

--- src/dertwothumber/frontend/api/repos.cljs
+++ src/dertwothumber/frontend/api/repos.cljs
@@ -1,7 +1,17 @@
 (ns dertwothumber.frontend.api.repos
+  (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [reagent.session :as session]
-            [cljs-http.client :as http]))
+            [cljs-http.client :as http]
+            [cljs.core.async :refer [<! chan >!]]
+            [cognitect.transit :as transit]))
 
 
 (defn fetch-repos []
-  (http/get "/api/repos"))
+  (let [response-channel (chan 1)
+        reader (transit/reader :json)]
+    (go
+      (let [response (<! (http/get "/api/repos"))
+            body     (:body response)]
+      (>! response-channel (transit/read reader body))))
+    response-channel))
+
