268f856d0e1cfe4beb4fd56ec3df47339ea51ade
Topper Bowers <topper@toppingdesign.com>
2016-03-27 20:08:59 -0400

    add a mocking framework (conjure) and test that repos get created with the right calls

--- test/dertwothumber/component/repo_test.clj
+++ test/dertwothumber/component/repo_test.clj
@@ -2,7 +2,9 @@
   (:require [clojure.test :refer :all]
             [com.stuartsierra.component :as component]
             [dertwothumber.component.dynamo-db :as dynamo-db]
-            [dertwothumber.component.repo :as repo]))
+            [dertwothumber.component.repo :as repo]
+            [conjure.core :refer [mocking verify-call-times-for verify-first-call-args-for]]
+            [tentacles.repos]))
 
 (defn new-repo-system []
   (let [test-config {:access-key "<AWS_DYNAMODB_ACCESS_KEY>"
@@ -28,4 +30,13 @@
     (testing "creating a repo"
       (repo/create-repo repo {:full-name "tobowers/test-repo" :url "http://something"}))
     (testing "fetching a repo"
-      (is (= "http://something" (:url (repo/get-repo repo "tobowers/test-repo")))))))
+      (is (= "http://something" (:url (repo/get-repo repo "tobowers/test-repo")))))
+    (testing "creating repo hook"
+      (mocking [tentacles.repos/create-hook]
+        (repo/create-hook repo "user-id" "repo-name" "access-token")
+        (verify-call-times-for tentacles.repos/create-hook 1)
+        (verify-first-call-args-for tentacles.repos/create-hook "user-id" "repo-name" "web"
+                                                                {:url "http://localhost/webhooks"}
+                                                                {:active true
+                                                                 :events [:pull_request_review_comment :push :pull_request]
+                                                                 :oauth-token "access-token"})))))
