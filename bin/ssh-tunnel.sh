a33e1ca51121a1398649b3bc862984baafbc90f4
Topper Bowers <topper@toppingdesign.com>
2016-03-29 20:20:37 -0400

    support linux grok too

--- bin/ssh-tunnel.sh
+++ bin/ssh-tunnel.sh
@@ -1,2 +1,11 @@
 #!/usr/bin/env bash
-exec ngrok http 192.168.99.100:3000
+
+command_exists () {
+    type "$1" &> /dev/null ;
+}
+
+if command_exists docker-machine; then
+  exec ngrok http 192.168.99.100:3000
+else
+  exec ngrok http localhost:3000
+fi
