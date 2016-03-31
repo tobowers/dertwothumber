dccb2ae791802e6892dce63c33dd7e91ff1919cd
Topper Bowers <topper@toppingdesign.com>
2016-03-30 22:51:45 -0400

    see if accepted and set state of a pull request

--- src/dertwothumber/pull_request.clj
+++ src/dertwothumber/pull_request.clj
@@ -3,9 +3,17 @@
             [tentacles.issues]
             [tentacles.core :refer [api-call]]))
 
+(def thumbs-up-regex #"\:\+1\:")
+
 (defprotocol GithubFunctions
   (get-comments [pull-request] "fetch the comments for a pull-request (for some reason this uses the issue API)")
-  (set-status [pull-request status] "set the status of a pull-request "))
+  (set-state [pull-request state] "set the status of a pull-request "))
+
+(defprotocol AcceptanceFunctions
+  (is-accepted? [pull-request] "if the pull request passes dertwothumber this is true"))
+
+(defn- comment-has-thumb? [comment]
+  (re-find thumbs-up-regex (:body comment)))
 
 ; see https://developer.github.com/v3/activity/events/types/#pullrequestevent for the data structure
 
@@ -16,7 +24,17 @@
           repo-name (get-in pull-request [:pull_request :base :repo :name])
           pr-id (:number pull-request)
           access-token (:access-token pull-request)]
-      (tentacles.issues/issue-comments user-id repo-name pr-id {:oauth-token access-token}))))
+      (tentacles.issues/issue-comments user-id repo-name pr-id {:oauth-token access-token})))
+
+  (set-state [pull-request state]
+    {:pre [(contains? #{:pending :success :error :failure} state)]}
+    (let [status-url (get-in pull-request [:pull_request :statuses_url])]
+      (api-call :post status-url {:state state
+                                  :context "code-review"
+                                  :description "awaiting thumbs"})))
+  AcceptanceFunctions
+  (is-accepted? [pull-request]
+    (some comment-has-thumb? (get-comments pull-request))))
 
 (defn pull-request [opts]
   (map->PullRequest opts))
--- test/dertwothumber/pull_request_test.clj
+++ test/dertwothumber/pull_request_test.clj
@@ -1,17 +1,34 @@
 (ns dertwothumber.pull-request-test
   (:require [clojure.test :refer :all]
-            [dertwothumber.pull-request :refer [pull-request get-comments]]
-            [conjure.core :refer [mocking verify-call-times-for verify-first-call-args-for]]
+            [dertwothumber.pull-request :refer [pull-request get-comments is-accepted? set-state]]
+            [conjure.core :refer [mocking stubbing verify-call-times-for verify-first-call-args-for]]
             [cheshire.core :refer :all]
             [tentacles.pulls]
+            [tentacles.core]
             [tentacles.issues]))
 
 (def test-pr-post
   (parse-string (slurp (clojure.java.io/resource "test_pr_post.json")) true))
 
-(deftest testing-pull-requests
-  (testing "it fetches comments"
-    (let [pr (pull-request (assoc test-pr-post :access-token "abc"))]
+(deftest acceptance-functions-tests
+  (let [pr (pull-request (assoc test-pr-post :access-token "abc"))]
+    (testing "is-accepted? true if a thumb"
+      (stubbing [tentacles.issues/issue-comments [{:body "test"}]]
+                (is (not (is-accepted? pr)))))
+    (testing "is-accepted? false if not thumb"
+      (stubbing [tentacles.issues/issue-comments [{:body ":+1:"}]]
+                (is (is-accepted? pr))))))
+
+(deftest github-function-tests
+  (let [pr (pull-request (assoc test-pr-post :access-token "abc"))]
+    (testing "it fetches comments"
       (mocking [tentacles.issues/issue-comments]
-        (get-comments pr)
-        (verify-call-times-for tentacles.issues/issue-comments 1)))))
+               (get-comments pr)
+               (verify-call-times-for tentacles.issues/issue-comments 1)))
+    (testing "setting state"
+      (mocking [tentacles.core/api-call]
+               (testing "with a pending state"
+                 (set-state pr :pending)
+                 (verify-call-times-for tentacles.core/api-call 1))))))
+
+
